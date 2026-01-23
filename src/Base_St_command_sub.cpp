1ff76d409dc6f2f8b65286b70a0299f2989a2095
Rtamanyu N J <rtamanyu@gmail.com>
2026-01-23 22:32:07 +0530

    Updated the subscriber unit test code to blink builtin LED to signal command recieved

--- src/Base_St_command_sub.cpp
+++ src/Base_St_command_sub.cpp
@@ -1,78 +1,73 @@
-// #include <micro_ros_arduino.h>
-// #include <rcl/rcl.h>
-// #include <rclc/rclc.h>
-// #include <rclc/executor.h>
-// #include <std_msgs/msg/int32_multi_array.h>
+#include <micro_ros_arduino.h>
+#include <rcl/rcl.h>
+#include <rclc/rclc.h>
+#include <rclc/executor.h>
+#include <std_msgs/msg/int32_multi_array.h>
 
-// rcl_node_t node;
-// rcl_subscription_t subscriber;
-// rclc_executor_t executor;
-// rcl_allocator_t allocator;
-// rclc_support_t support;
+rcl_node_t node;
+rcl_subscription_t subscriber;
+rclc_executor_t executor;
+rcl_allocator_t allocator;
+rclc_support_t support;
 
-// std_msgs__msg__Int32MultiArray msg;
-// int32_t data_buffer[5];
+std_msgs__msg__Int32MultiArray msg;
+int32_t data_buffer[5];
 
-// void subscription_callback(const void *msgin)
-// {
-//   const std_msgs__msg__Int32MultiArray *msg =
-//     (const std_msgs__msg__Int32MultiArray *)msgin;
+void subscription_callback(const void *msgin)
+{
+  const std_msgs__msg__Int32MultiArray *msg =
+    (const std_msgs__msg__Int32MultiArray *)msgin;
 
-//   Serial.println("Received Science Command:");
+bool led_on = false;
+if (msg->data.data[0] == 1) led_on = true;
+if (msg->data.data[1] == 1) led_on = true;
+if (msg->data.data[2] == 1) led_on = true;
+if (msg->data.data[3] == 1) led_on = true;
+if (msg->data.data[4] == 1) led_on = true;
 
-//   if (msg->data.data[0] == 1) Serial.println("Drill: UP");
-//   else if (msg->data.data[0] == -1) Serial.println("Drill: DOWN");
+digitalWrite(LED_BUILTIN, led_on ? HIGH : LOW);
+}
 
-//   if (msg->data.data[1] == 1) Serial.println("Drill Speed: INCREASE");
-//   else if (msg->data.data[1] == -1) Serial.println("Drill Speed: DECREASE");
+void setup()
+{
+  Serial.begin(115200);
 
-//   if (msg->data.data[2] == 1) Serial.println("Barrel: ACTIVATED");
+  // WAIT for USB CDC to be ready (CRITICAL)
+  while (!Serial && millis() < 8000) {
+    delay(10);
+  }
 
-//   if (msg->data.data[3] == 1) Serial.println("Servo: TOGGLE");
+  pinMode(LED_BUILTIN, OUTPUT);
 
-//   if (msg->data.data[4] == 1) Serial.println("Science Exploration: TOGGLE");
+  delay(2000);
 
-//   Serial.println("----------------------");
-// }
+  set_microros_transports();
 
-// void setup()
-// {
-//   Serial.begin(115200);
+  allocator = rcl_get_default_allocator();
+  rclc_support_init(&support, 0, NULL, &allocator);
 
-//   // WAIT for USB CDC to be ready (CRITICAL)
-//   while (!Serial && millis() < 8000) {
-//     delay(10);
-//   }
+  rclc_node_init_default(&node, "teensy_science_subscriber", "", &support);
 
-//   delay(2000);
+  rclc_subscription_init_default(
+    &subscriber,
+    &node,
+    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32MultiArray),
+    "/science_control");
 
-//   set_microros_transports();
+  msg.data.data = data_buffer;
+  msg.data.size = 5;
+  msg.data.capacity = 5;
 
-//   allocator = rcl_get_default_allocator();
-//   rclc_support_init(&support, 0, NULL, &allocator);
+  rclc_executor_init(&executor, &support.context, 1, &allocator);
+  rclc_executor_add_subscription(
+    &executor,
+    &subscriber,
+    &msg,
+    &subscription_callback,
+    ON_NEW_DATA);
+}
 
-//   rclc_node_init_default(&node, "teensy_science_subscriber", "", &support);
-
-//   rclc_subscription_init_default(
-//     &subscriber,
-//     &node,
-//     ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32MultiArray),
-//     "/science_control");
-
-//   msg.data.data = data_buffer;
-//   msg.data.size = 5;
-//   msg.data.capacity = 5;
-
-//   rclc_executor_init(&executor, &support.context, 1, &allocator);
-//   rclc_executor_add_subscription(
-//     &executor,
-//     &subscriber,
-//     &msg,
-//     &subscription_callback,
-//     ON_NEW_DATA);
-// }
-
-// void loop()
-// {
-//   rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100));
-// }
+void loop()
+{
+  rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100));
+}
